a9eb193f71e3276d44613f211900844e91f3b8f0
ahmadfauzi <ahmadfauzi.alwahid@gmail.com>
2015-04-25 20:52:02 +0700

    FTDetailFragment part1

---
+++ app/src/main/java/com/example/ahmadfauzi/testsqlitedb/dashboard_foodtest/FTDetailFragment.java
@@ -0,0 +1,52 @@
+package com.example.ahmadfauzi.testsqlitedb.dashboard_foodtest;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.example.ahmadfauzi.testsqlitedb.R;
+import com.example.ahmadfauzi.testsqlitedb.model.FoodTest;
+import com.example.ahmadfauzi.testsqlitedb.model.MySQLiteHelper;
+
+/**
+ * Created by 5111100057 on 4/25/2015.
+ */
+public class FTDetailFragment extends Fragment{
+    FoodTest foodTest;
+    EditText editTextName, editTextReagent, editTextResult;
+    ImageView ivFoodTest;
+    Button buttonChooseImage;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        Bundle bundle = getArguments();
+        if(bundle != null && bundle.containsKey(MySQLiteHelper.TABLE_NAME_FT)){
+            foodTest = new FoodTest(bundle);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.detail_ft_fragment, container, false);
+
+        ivFoodTest = (ImageView) view.findViewById(R.id.ivFoodTest);
+        editTextName = (EditText) view.findViewById(R.id.editTextName);
+        editTextReagent = (EditText) view.findViewById(R.id.editTextReagent);
+        editTextResult = (EditText) view.findViewById(R.id.editTextResult);
+
+        if(foodTest != null){
+            editTextName.setText(foodTest.getNameFT());
+            editTextReagent.setText(foodTest.getReagentFT());
+            editTextResult.setText(foodTest.getResultFT());
+        }
+        return view;
+    }
+}
--- app/src/main/java/com/example/ahmadfauzi/testsqlitedb/dashboard_foodtest/FTListFragment.java
+++ app/src/main/java/com/example/ahmadfauzi/testsqlitedb/dashboard_foodtest/FTListFragment.java
@@ -2,7 +2,7 @@ package com.example.ahmadfauzi.testsqlitedb.dashboard_foodtest;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import android.app.ListFragment;
 import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
@@ -24,7 +24,7 @@ public class FTListFragment extends ListFragment{
 
     }
 
-    @Override 
+    @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
